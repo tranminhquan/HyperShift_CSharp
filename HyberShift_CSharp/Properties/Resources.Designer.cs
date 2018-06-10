d322330beb88bd0902a9b684c62bd2920a9c59ab
Kiet Vo <geforce2409@gmail.com>
2018-06-10 22:57:28 +0700

    Add fix listRoom

--- HyberShift_CSharp/Properties/Resources.Designer.cs
+++ HyberShift_CSharp/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace HyberShift_CSharp.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon xing_logo {
+            get {
+                object obj = ResourceManager.GetObject("xing_logo", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
